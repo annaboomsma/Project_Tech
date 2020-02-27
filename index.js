daaf00c847f832733080a1f954e7d318d36a7d4f
admirai <isabel.admiraal@hva.nl>
2020-02-27 15:37:30 +0100

    Console logged the URL query data.

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const camelCase = require('camelcase');
 const app = express();
 const port = 3000;
 
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 
 // Serve static files from the folder  public
 // Load the files that are in the public directory from the /static path
@@ -20,10 +20,22 @@ app.get('/contact', (req, res) => {
     res.send('Contact us!')
 })
 
+// app.get('/about', (req, res) => {
+//     res.send('About me')
+    
+// })
+
+
+
+// WEEK 2
+
+// Wanneer /about wordt gerequest, log dan de query informatie die is meegestuurd
 app.get('/about', (req, res) => {
     res.send('About me')
+    console.log(req.query);
 })
 
 
+
 // Return text to camelcase and console.log it.
-console.log(camelCase('Hey-Isabel'));
+//console.log(camelCase('Hey-Isabel'));
