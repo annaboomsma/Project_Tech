aaac0e37177d6c8652d74e52808176a7211c299f
admirai <isabel.admiraal@hva.nl>
2020-03-15 19:15:07 +0100

    Updated :id route.

--- index.js
+++ index.js
@@ -4,26 +4,7 @@ const bodyParser = require('body-parser');
 const slug = require('slug');
 const multer = require('multer');
 const app = express();
-
-
-
-
-const port = 3000;
-// const upload = multer({dest: '/uploads'})
-
-
-// set the view engine to ejs
-app
-    .use(express.static(__dirname + '/public'))
-    .use(bodyParser.urlencoded({extended: true}))
-    .set('view engine', 'ejs')
-    .get('/', users)
-    .get('/:id',user);
-
-    
-
-
-// //  WEEK 3
+const port = 3001;
 
 
 const profiles = [
@@ -41,20 +22,65 @@ const profiles = [
     }
   ]
 
-  function users(req, res) {
-    res.render('pages/users', {data: profiles})
 
-  }
+// const upload = multer({dest: '/uploads'})
 
-function user(req,res ,next){
-//console.log(req.params.id)
-//const profile =  req.params.id;
-const profile = profiles.find(user=>user.id===req.params.id)
-res.render('pages/profile', {info : profile});
 
+// set the view engine to ejs
+app
+    .use(express.static(__dirname + '/public'))
+    .use(bodyParser.urlencoded({extended: true}))
+    .set('view engine', 'ejs')
+    .get('/users', users)
+    .get('/user/:id',user)
+    .get('/signup', form)
+    .post('/signup', create);
+
+
+
+
+// //  WEEK 3
+
+function form(req, res) {
+    res.render('pages/upload');
 }
-  
+
+
+function users(req, res) {
+    res.render('pages/users', {data: profiles})
+    //res.send(profiles)
+}
+
+function create(req, res) {
+  var id = slug(req.body.name).toLowerCase()
+
+  profiles.push({
+      id: id,
+      name: req.body.name,
+      residence: req.body.residence,
+      description: req.body.description
+  })
+
+  res.redirect('/user/' + id)
+}
+
+function user(req,res){
+    const id =  req.params.id;
+    const profile = profiles.find(user=>user.id===id)
+
+     res.render('pages/profile', {info : profile});
+    //res.send(profile)
+}
+
+
+// function edit(req,res){
+//   const id =  req.params.id;
+//   const profile = profiles.find(user=>user.id===id)
+
+//    res.render('pages/edit_profile', {title: 'Edit profile' ,info : profile});
+//   //res.send(profile)
+//   res.redirect('/user/edit' + id)
+// }
+
   
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-
