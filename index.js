a43dc993bb9db7a1c3a648a9f406fdacda93c076
admirai <isabel.admiraal@hva.nl>
2020-03-16 16:25:23 +0100

    Insert data and send into database.

--- index.js
+++ index.js
@@ -3,27 +3,95 @@ const camelCase = require('camelcase');
 const bodyParser = require('body-parser');
 const slug = require('slug');
 const multer = require('multer');
-const routes = require('./routes.js')
+//const routes = require('./routes.js')
 const app = express();
 const port = 3001;
 const mongo = require('mongodb');
+const mongoose = require('mongoose');
 require('dotenv').config();
-let url =  process.env.DB_URL;
+let url = process.env.DB_URL;
 
 
 app
-    .use(express.static(__dirname + '/public'))
-    .use(bodyParser.urlencoded({extended: true}))
-    .set('view engine', 'ejs')
-    .use(routes)
+  .set('view engine', 'ejs')
+  .use(express.static(__dirname + '/public'))
+  .use(bodyParser.urlencoded({
+    extended: true
+  }))
+  //.use(routes)  
+  .get('/', form)
+  .post('/insert', insert)
+  .get("/:id", profile);
 
-   
-    mongo.MongoClient.connect(url, function (err, client) {
-      if (err) {
-        throw err;
-      }
-      db = client.db(process.env.DB_NAME);
-      console.log('Connected to database');
 
-    })
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+function insert(req, res, next) {
+  db.collection('user_data').insertOne({
+    name: req.body.name,
+    age: req.body.age,
+    residence: req.body.residence,
+    gender: req.body.residence,
+    interest: req.body.interest,
+    music_genre: req.body.genre
+  }, completed)
+
+  function completed(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/' + data.insertedId)
+    }
+  }
+}
+
+function profile(req, res, next) {
+  const id = req.params.id
+  db.collection('users_data').findOne({
+    id: id
+  }, done)
+
+
+  function done(err, data) {
+    if (err) {
+      next(err);
+    } else {
+      res.render('pages/profile', {
+        data: data
+      });
+    }
+  }
+}
+
+function form(req, res) {
+  res.render('pages/upload');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+mongo.MongoClient.connect(url, function (err, client) {
+  if (err) {
+    throw err;
+  }
+  db = client.db(process.env.DB_NAME);
+
+
+})
+
+
+
+
+
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
