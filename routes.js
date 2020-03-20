f7324a4bbab338df0d3d3f157f1819a004bf7cb2
admirai <isabel.admiraal@hva.nl>
2020-03-20 01:47:08 +0100

    Fixed repo structure and made little changes in ejs templates.

--- routes.js
+++ routes.js
@@ -1,81 +1,46 @@
 const express   = require('express')
 const router    = new express.Router()
 const slug = require('slug');
+const url  = require('./index.js')
 
-// dummy data 
-let profiles = [
-    {
-      id: 'isabel-admiraal',
-      name: 'Isabel Admiraal',
-      residence: 'Alkmaar',
-      description: 'Love for hip-hop and r&b'
-    },
-    {
-      id: 'esmay-baay',
-      name: 'Esmay Baay',
-      residence: 'Alkmaar',
-      description: 'Scorpio who is enjoying life'
-    }
-  ]
 
-  // routes with arrow functions. 
 router  
-    .get('/', (req,res)=>{
-        res.render('pages/upload');
+   .get('/', (req, res ,next)=>{
+        
+             
     })
-
-    .get('/users', (req,res)=>{
-        res.render('pages/users', {data: profiles})
-        //res.send(profiles)
+    
+    .get('/profile', (req, res ,next)=>{
+        
+        
         
     })
 
-    .get('/user/:id', (req,res)=>{
-        const id =  req.params.id;
-        const profile = profiles.find(user=>user.id===id)
+    // This route/function purpose is to store the users input data
+    // Inserted via the ejs form.
+    .post('/insert', (req, res ,next)=>{
+    const profile = {
+        name: req.body.name,
+        age: req.body.age,
+        residence: req.body.residence,
+        gender: req.body.residence,
+        interest: req.body.interest,
+        music_genre: req.body.genre
+    }; 
     
-        // render profile with the matching data.
-         res.render('pages/profile', {info : profile});
-        //res.send(profile)
+    res.redirect('/')
+    
     })
-
-    .get('/signup', (req,res)=>{
-        res.render('pages/upload');
-    })
-
-    .post('/signup', (req,res)=>{
-        // convert to lowercase
-        var id = slug(req.body.name).toLowerCase()
+    .post('/update', (req, res ,next)=>{
         
-        //push the body data into the profiles array
-        profiles.push({
-            id: id,
-            name: req.body.name,
-            residence: req.body.residence,
-            description: req.body.description
-        })
-      
-        // redirect to the corresponding user id page
-        res.redirect('/user/' + id)
+        
+        
+    })
+    .post('/delete', (req, res ,next)=>{
+        
+        
+        
     })
 
-    
-    .post('/user/:id', (req,res)=>{
-        // post the updated profile info to the profile
-        var id = slug(req.body.name).toLowerCase()
 
-        profiles.push({
-            id: id,
-            name: req.body.name,
-            residence: req.body.residence,
-            description: req.body.description
-        })
-
-       // redirect to the corresponding user id page
-        res.redirect('/user/' + id)
-    })
-
-    
-
-// export routes, so I cans use it in my index.js
 module.exports = router
